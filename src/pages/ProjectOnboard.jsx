a945baa8ee43afdc49e790be000b3fbff92216b6
Gretta Wallace <grettawallacemail@gmail.com>
2022-12-27 17:01:57 -0500

    added project onboard page content

--- src/pages/ProjectOnboard.jsx
+++ src/pages/ProjectOnboard.jsx
@@ -1,4 +1,6 @@
 import '../styles/Home.css';
+import { Icon } from '@iconify/react';
+
 
 function ProjectOnboard() {
     return (
@@ -14,8 +16,8 @@ function ProjectOnboard() {
                     <nav className="navbar">
                     </nav>
 
-                    <div className="navbutton">
-                        <span className="iconify" data-icon="bx-bxs-user-circle" data-inline="false" style="width:50px;height:50px;color:#cccccc;padding:20px 0px;"></span><br />
+                    <div className="navbutton" style={{top: '-13px'}}>
+                        <Icon icon="bx-bxs-user-circle" style={{ fontSize: '50px', color: '#cccccc', padding: '20px 0px' }} /><br />
 
                     </div>
 
@@ -32,26 +34,26 @@ function ProjectOnboard() {
 
 
 
-            <div className="contentsection center" style="margin-top:0px;">
+            <div className="contentsection center" style={{ marginTop: '0px' }}>
 
                 <h1>Get Started with a Project</h1>
                 <p>Don't worry. This project doesn't have to be a gem.<br />We won't show it to anyone until it's refined and published</p>
 
-                {/* <form>
-                    <input type="text" placeholder="Project Name" className="forty"><br />
-                        <input type="text" placeholder="A Short Description of Your Project" className="forty multitext"><br />
-                            <input type="submit" className="blueback" value="Next Step">
-                            </form> */}
+                <form>
+                    <input type="text" placeholder="Project Name" className="forty" /><br />
+                    <input type="text" placeholder="A Short Description of Your Project" className="forty multitext" /><br />
+                    <input type="submit" className="blueback" value="Next Step" />
+                </form>
             </div>
 
-                        <section>
+            <section>
 
-                            <div className="contentsection">
-                            </div>
+                <div className="contentsection">
+                </div>
 
-                        </section>
-                    </>
-                    );
+            </section>
+        </>
+    );
 }
 
-                    export default ProjectOnboard;
+export default ProjectOnboard;
